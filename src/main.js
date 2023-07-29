b742702d4bd61ae1913308659ee7ae7b3971ecf2
zulfahmidev <developer.zulfahmi@gmail.com>
2023-07-29 22:53:50 +0700

    fix bug

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import axios from 'axios'
 import store from './store.js'
 // import pusher from 'vue-pusher';
 
-// axios.defaults.baseURL = 'https://api.vocaject.com/api'
+axios.defaults.baseURL = 'https://api.vocaject.com/api'
 // axios.defaults.baseURL = 'http://34.101.227.89/api'
-axios.defaults.baseURL = 'http://localhost:8000/api'
+// axios.defaults.baseURL = 'http://localhost:8000/api'
 
 let app = createApp(App)
 .use(router)
