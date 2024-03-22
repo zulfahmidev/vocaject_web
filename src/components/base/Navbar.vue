249363126e17ae2743b07b1602b124f42034fedd
zulfahmidev <developer.zulfahmi@gmail.com>
2024-03-22 16:57:19 +0700

    membuat halaman setting profile

--- src/components/base/Navbar.vue
+++ src/components/base/Navbar.vue
@@ -10,16 +10,31 @@
       <!-- Profile -->
       <div class="flex gap-4 items-center">
         <div class="text-right">
-          <div class="text-sm">
-            Politeknik Negeri Lhokseumawe
+          <div class="text-sm capitalize">
+            {{ $store.state.user.name }}
           </div>
           <div class="text-xs text-slate-500">
-            Rp. 100,000
+            {{ formatRupiah($store.state.user.balance) }}
           </div>
         </div>
-        <div class="h-12 w-12 bg-slate-100 rounded-full"></div>
+        <div class="h-12 w-12 bg-slate-100 rounded-full overflow-hidden">
+          <img :src="$store.state.user.picture" :alt="$store.state.user.name">
+        </div>
       </div>
 
     </div>
   </nav>
 </template>
+
+<script type="ts">
+export default {
+  methods: {
+    formatRupiah(number) {
+      return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR"
+      }).format(number);
+    }
+  }
+}
+</script>
