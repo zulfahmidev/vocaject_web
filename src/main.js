609361e0ded0fe0b77b6833bb40debab85da105e
zulfahmidev <developer.zulfahmi@gmail.com>
2024-05-06 11:28:31 +0700

    asdasd

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 // import { defineConfig, loadEnv } from 'vite'
 
-// axios.defaults.baseURL = 'https://mobile.vocaject.com/api'
-axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.baseURL = 'https://mobile.vocaject.com/api'
+// axios.defaults.baseURL = 'http://localhost:8000/api'
 
 let app = createApp(App)
 .use(VueSweetalert2)
