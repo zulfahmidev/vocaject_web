55f587587fec6fcbad1e6f39ef8646c4ee43d316
zulfahmidev <developer.zulfahmi@gmail.com>
2024-05-11 14:13:31 +0700

    memperbaiki component project

--- src/components/cards/Project.vue
+++ src/components/cards/Project.vue
@@ -1,18 +1,18 @@
 <template>
   <router-link :to="{name: 'progress-project', params: {id: data?.id}}" :class="`block rounded shadow p-3 button-hover ${active ? 'button-active' : 'bg-white'}`">
-    <div class="h-36 rounded bg-green-100 flex items-center justify-center">
-      <img src="/assets/icondesain.png" class="h-28" alt="category icon">
+    <div class="flex gap-2">
+      <div class="h-20 w-20 rounded bg-green-100 flex items-center justify-center">
+        <img src="/assets/icondesain.png" class="h-1/2" alt="category icon">
+      </div>
+      <div class="py-1">
+        <h2 class="capitalize">{{ data?.title }}</h2>
+        <p class="text-xs mt-1 inline text-slate-500">{{ data?.description }}</p>
+      </div>
     </div>
     <div class="mt-3">
-      <div class="flex justify-between items-start">
-        <h2 class="capitalize">{{ data?.title }}</h2>
-        <div v-if="data?.status == 'opened'" class="badge-xs border-green-300 text-green-700 bg-green-200">Buka</div>
-        <div v-if="data?.status == 'completed'" class="badge-xs border-blue-300 text-blue-700 bg-blue-200">Selesai</div>
-        <div v-if="data?.status == 'closed'" class="badge-xs border-red-300 text-red-700 bg-red-200">Tutup</div>
-      </div>
-      <p class="text-xs mt-1 inline text-slate-500">{{ data?.description }}</p>
+      
       <div class="mt-3 flex justify-between">
-        <div class="text-xs">
+        <div class="text-xs" v-if="hidePercent">
           <i class="fa fa-fw fa-money-bill mr-1"></i>
           <span>Rp. {{ data?.budget }}</span>
         </div>
@@ -23,6 +23,11 @@
         <div class="text-xs" v-if="!hidePercent">
           <i class="fa fa-fw fa-bars-progress mr-1"></i>
           <span>{{ data?.progress }}%</span>
+        </div>
+        <div class="flex justify-between items-start" v-if="!hidePercent">
+          <div v-if="data?.status == 'opened'" class="badge-xs border-green-300 text-green-700 bg-green-200">Buka</div>
+          <div v-if="data?.status == 'completed'" class="badge-xs border-blue-300 text-blue-700 bg-blue-200">Selesai</div>
+          <div v-if="data?.status == 'closed'" class="badge-xs border-red-300 text-red-700 bg-red-200">Tutup</div>
         </div>
       </div>
     </div>
