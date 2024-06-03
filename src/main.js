3f9b4515f578c64c023253f8c0a6b3161837c1ac
zulfahmidev <developer.zulfahmi@gmail.com>
2024-06-03 11:36:34 +0700

    update api

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 // import { defineConfig, loadEnv } from 'vite'
 
-axios.defaults.baseURL = 'https://api.vocaject.com/api'
+axios.defaults.baseURL = 'https://staging.vocaject.com/api'
+// axios.defaults.baseURL = 'https://api.vocaject.com/api'
 // axios.defaults.baseURL = 'http://localhost:8000/api'
 
 let app = createApp(App)
