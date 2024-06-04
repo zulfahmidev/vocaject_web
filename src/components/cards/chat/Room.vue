69eb0550af99e207361261ede48a5d46d13da6b1
zulfahmidev <developer.zulfahmi@gmail.com>
2024-06-04 12:04:57 +0700

    halah

--- src/components/cards/chat/Room.vue
+++ src/components/cards/chat/Room.vue
@@ -249,7 +249,8 @@ export default {
 
       await this.channel.bind('new-message', ({ data }) => {
         if (data.project.id == this.project_id && data.lecture.id == this.contact_id && data.lecture_id != this.$store.state.user.id) {
-          if (data.sender != this.$store.state.user.role) {
+          let sender = (data.sender == 'college') ? 'company' : data.sender;
+          if (sender != this.$store.state.user.role) {
             data.message = JSON.parse(data.message);
             this.messages.push(data);
             setTimeout(() => {
